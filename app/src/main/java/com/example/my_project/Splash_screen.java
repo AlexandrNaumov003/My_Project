9377b1bbd91947b8f9ec253c0bf64cdb4c5d977c
AlexandrNaumov003 <AlexandrNaumov003@yandex.ru>
2022-01-02 12:45:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/my_project/Splash_screen.java
@@ -0,0 +1,16 @@
+package com.example.my_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/my_project/Statistic_screen.java
@@ -0,0 +1,14 @@
+package com.example.my_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Statistic_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistic_screen);
+    }
+}
