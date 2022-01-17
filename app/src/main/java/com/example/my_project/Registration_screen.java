1885bdfc4d528f2934bdee8a886e55b99553e05e
AlexandrNaumov003 <AlexandrNaumov003@yandex.ru>
2022-01-17 17:43:48 +0200

    17.01

--- app/src/main/java/com/example/my_project/Registration_screen.java
+++ app/src/main/java/com/example/my_project/Registration_screen.java
@@ -1,14 +1,54 @@
 package com.example.my_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Registration_screen extends AppCompatActivity {
+
+    EditText name, username, email, password;
+    Button register;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_screen);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        name = findViewById(R.id.et_name);
+        username = findViewById(R.id.et_username);
+        email = findViewById(R.id.et_email);
+        password = findViewById(R.id.et_password);
+        register = findViewById(R.id.btn_register);
+        mAuth = FirebaseAuth.getInstance();
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Registration_screen.this, "User registered", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        Toast.makeText(this, mAuth.getCurrentUser().getEmail(), Toast.LENGTH_SHORT).show();
+
     }
 }
